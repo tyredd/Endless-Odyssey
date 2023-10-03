34ee948ffa6ef4c7fc2e341be4b76386073c9da1
tyredd <146857057+tyredd@users.noreply.github.com>
2023-10-03 13:43:36 -0400

    Animation

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,11 +10,13 @@ public class Player : MonoBehaviour
     public float speed = 10.0f;
     public GameObject arrowPrefab;
     public Transform arrowSpawn;
+    private Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,6 +27,7 @@ public class Player : MonoBehaviour
         transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector2.up * verticalInput * speed * Time.deltaTime);
+        anim.SetFloat("Horizontal", horizontalInput);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
